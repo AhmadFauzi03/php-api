ef726e413080f042433da6bc72d4b6907aa79287
Ahmad Fauzi <ahmadfauzi282002@gmail.com>
2018-10-31 15:51:43 +0700

    delete_data

---
+++ delete.php
@@ -0,0 +1,8 @@
+<?php 
+include 'aksi.php';
+$id = $_GET['id'];
+$sql_insert="DELETE FROM users WHERE id='$id'";
+mysqli_query($conn,$sql_insert)or die(mysqli_error($conn));
+ 
+header("location:index.php?pesan=hapus");
+?> 
